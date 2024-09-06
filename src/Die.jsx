ef3382a384f06e805aff8b333eb3039a130fbc54
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-09-06 22:13:23 +0100

    Update to the Die style

--- src/Die.jsx
+++ src/Die.jsx
@@ -2,9 +2,25 @@ function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+
+    const pip = () => <span className="pip"></span>
+
+
     return (
-      <button style={styles} className="Die" onClick={props.handleClick}>{props.value}</button>
+        <div style={styles} className="die-face" onClick={props.handleClick}>
+            {Array.from({ length: props.value }).map((_, i) => pip())}
+        </div>
     )
-  }
-  
-  export default Die
+}
+
+export default Die
+/* function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    return (
+        <button style={styles} className="Die" onClick={props.handleClick}>{props.value}</button>
+    )
+}
+
+export default Die */
