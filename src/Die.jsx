19ece375cf9b016d0d6c53af92c8d86ad363f4ca
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-09-06 23:24:03 +0100

    Added a key for the pip's

--- src/Die.jsx
+++ src/Die.jsx
@@ -4,11 +4,11 @@ function Die(props) {
         boxShadow: props.isHeld ? "inset 0 3px #d7ffde, inset 0 -3px #66ae73, inset 3px 0 #96ce9a, inset -3px 0 #96ce9a" : "inset 0 3px white, inset 0 -3px #bbbbbb, inset 3px 0 #d7d7d7, inset -3px 0 #d7d7d7"
     }
 
-    const pip = () => <span className="pip"></span>
+    const pip = (key) => <span key={key} className="pip"></span>
 
     return (
         <div style={faceStyles} className="die-face" onClick={props.handleClick}>
-            {Array.from({ length: props.value }).map((_, i) => pip())}
+            {Array.from({ length: props.value }).map((_, i) => pip(i))}
         </div>
     )
 }
