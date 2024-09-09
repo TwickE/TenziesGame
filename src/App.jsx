ddc72b4a235e8d0f8cb29de5740fba4eb4497248
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-09-09 16:01:12 +0100

    Added a score system for the number of rolls and time

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Die from './Die'
+import Header from './Header'
 import {nanoid} from 'nanoid'
 import Confetti from 'react-confetti'
 
 function App() {
     const [diceArray, setDiceArray] = React.useState(allnNewDice())
     const [tenzies, setTenzies] = React.useState(false)
+    const [time, setTime] = React.useState(0)
+    const [timerStarted, setTimerStarted] = React.useState(false);
+    const [rolls, setRolls] = React.useState(0)
+    const [bestScore, setBestScore] = React.useState({ rolls: Infinity, time: Infinity });
 
     const appRef = React.useRef(null);
     const [appWidth, setAppWidth] = React.useState(0);
@@ -17,13 +22,39 @@ function App() {
     }, []);
 
     React.useEffect(() => {
+        const storedBestScore = JSON.parse(localStorage.getItem('bestScore'));
+        if (storedBestScore) {
+            setBestScore(storedBestScore);
+        }
+    }, []);
+
+    React.useEffect(() => {
         const isTenzies = diceArray.every(die =>
              die.value === diceArray[0].value && die.isHeld
         )
         if (isTenzies) {
             setTenzies(true)
+            setTimerStarted(false)
+
+            if (rolls < bestScore.rolls || (rolls === bestScore.rolls && time < bestScore.time)) {
+                const newBestScore = { rolls, time };
+                setBestScore(newBestScore);
+                localStorage.setItem('bestScore', JSON.stringify(newBestScore));
+            }
         }
-    }, [diceArray])
+    }, [diceArray, rolls, time, bestScore])
+
+    React.useEffect(() => {
+        let timer;
+        if (timerStarted && !tenzies) {
+            timer = setInterval(() => {
+                setTime(prevTime => prevTime + 1);
+            }, 1000);
+        } else {
+            clearInterval(timer);
+        }
+        return () => clearInterval(timer);
+    }, [timerStarted, tenzies]);
 
     function generateNewDie() {
         return {
@@ -45,16 +76,23 @@ function App() {
         if(tenzies) {
             setDiceArray(allnNewDice())
             setTenzies(false)
+            setRolls(0)
+            setTime(0);
+            setTimerStarted(false);
         } else {
             setDiceArray(diceArray.map(die => {
                 return die.isHeld ? 
                     die :
                     generateNewDie()
             }))
+            setRolls(prevRolls => prevRolls + 1)
         }
     }
 
     function holdDice(id) {
+        if (!timerStarted) {
+            setTimerStarted(true); // Start the timer when a die is clicked for the first time
+        }
         setDiceArray(diceArray.map((die) => {
             if (die.id === id) {
                 return {
@@ -73,14 +111,17 @@ function App() {
     return (
         <main ref={appRef}>
             {tenzies && <Confetti width={appWidth} />}
-            <section>
-                <h1>Tenzies Game</h1>
-                <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            </section>
-            <div className="container-dies">
-                {diceElements}
+            <Header rolls={rolls} time={time} bestRolls={bestScore.rolls} bestTime={bestScore.time}/>
+            <div className='game-info'>
+                <section className='tenzies-info'>
+                    <h1>Tenzies Game</h1>
+                    <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+                </section>
+                <div className='container-dies'>
+                    {diceElements}
+                </div>
+                <button className='roll-btn' onClick={roll}>{tenzies ? "New Game" : "Roll"}</button>
             </div>
-            <button className='roll-btn' onClick={roll}>{tenzies ? "New Game" : "Roll"}</button>
         </main>
     )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+import crown from './assets/crown.svg'
+
+function Header(props) {
+    return(
+        <section className='header'>
+                <div className='score-item-container'>
+                    <img className='score-icon' src={crown} alt="crown icon" />
+                    <h2 className='score-item' style={{gridArea: '1 / 1 / 2 / 2'}}>Best Rolls Score: {props.bestRolls}</h2>
+                    <img className='score-icon' src={crown} alt="crown icon" />
+                </div>
+                <h2 className='score-item' style={{gridArea: '2 / 1 / 3 / 2'}}>Current Rolls: {props.rolls}</h2>
+                <div className='score-item-container'>
+                    <img className='score-icon' src={crown} alt="crown icon" />
+                    <h2 className='score-item' style={{gridArea: '1 / 2 / 2 / 3'}}>Best Time Score: {props.bestTime}s</h2>
+                    <img className='score-icon' src={crown} alt="crown icon" />
+                </div>
+                <h2 className='score-item' style={{gridArea: '2 / 2 / 3 / 3'}}>Current Time: {props.time}s</h2>
+        </section>
+    )
+}
+
+export default Header
